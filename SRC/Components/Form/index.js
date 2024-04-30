a8bee1e9fc548fd4d31e28e36bfc3ec4d8a82e14
kauansiqueira-cps <kauan.siqueira2@etec.sp.gov.br>
2024-04-30 12:18:29 -0300

    Aula 3004 - Função Calcular

--- SRC/Components/Form/index.js
+++ SRC/Components/Form/index.js
@@ -1,30 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TextInput, Button } from "react-native";
-import { validate } from "react-native-web/dist/cjs/exports/StyleSheet/validate";
+import ResultImc from "./ResultImc";
 
 export default function Form(){
+
+    const [height,setHeight] = useState(null)
+    const [weight,setWeight] = useState(null)
+    const [message,setMessage] = useState("Preencha o Peso e Altura");
+    const [imc, setImc] = useState(null)
+    const [textButton, setTextButton] = useState("Calcular IMC")
+
+        function imcCalcular(){
+            return setImc((weight/(height*height)).toFixed(2))
+        }
+
+        function validationImc(){
+            if (weight != null && height != null) {
+                imcCalcular()
+                setHeight(null)
+                setWeight(null)
+                setMessage("Seu Imc é = ")
+                setTextButton("Calcular Novamente")
+                return
+            } 
+            setImc(null)
+            setMessage("Preencha o Peso e Altura")
+        }
     return(
         <View>
-            <View>
-                <Text>Height: </Text>
-                <TextInput 
-                    placeholder="1.75"
-                    keyboardType="numeric"
-                />
-                <Text>Weight: </Text>
-                <TextInput
-                    placeholder="75.865"
-                    keyboardType="numeric"
-                /> 
 
-                <Button
-                onPress={validation()}
-                title="Calcular IMC"
-                color="#61dafb"
-                />
 
-                
-            </View>
+        <View>
+            <Text>Height: </Text>
+            <TextInput 
+                placeholder="Ex. 1.75"
+                keyboardType="numeric"
+            />
+            <Text>Weight: </Text>
+            <TextInput
+                placeholder="75.865"
+                keyboardType="numeric"
+            /> 
+
+            <Button
+            onPress={() => validationImc}
+            title="Calcular IMC"
+            color="#61dafb"
+            />
+
+        </View>
         </View>
     );
 }
