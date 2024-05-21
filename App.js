4845d013b1d0ae4384dc46fb2b349fcfb041cb85
kauansiqueira-cps <141190011+kauansiqueira-cps@users.noreply.github.com>
2024-05-21 12:16:29 -0300

    Styles

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '##3B39369',
+    backgroundColor: '#3B3936',
     paddingTop: 80,
   },
 });
